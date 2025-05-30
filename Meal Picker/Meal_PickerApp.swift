844c327498ff63f12780f8b2a22f00d12d55405d
hkem <hkem.moh.ar@gmail.com>
2025-05-30 19:58:52 +0800

    Initial Commit

---
+++ Meal Picker/Meal_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_PickerApp.swift
+//  Meal Picker
+//
+//  Created by Abdulhakim Aloraini on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Meal_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meal PickerTests/Meal_PickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_PickerTests.swift
+//  Meal PickerTests
+//
+//  Created by Abdulhakim Aloraini on 30/05/2025.
+//
+
+import Testing
+@testable import Meal_Picker
+
+struct Meal_PickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
